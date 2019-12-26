85e50238673212a7b57bd42b55df900d7b14035b
williammac2004 <williammacintosh2004@gmail.com>
2019-12-26 11:41:02 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,9 @@
 echo What is your video file called?
 read video
-echo How many FPS do you want?
+echo How many FPS do you want? (Usually 30 or 60)
 read fps
 
 ffmpeg -i $video %01d.jpg -hide_banner
-
 
 list=`ls | wc -l`
 one=2
@@ -22,11 +21,7 @@ done > bad.sh
 chmod +x bad.sh
 ./bad.sh
 
-
-
 ffmpeg -r 1 -pattern_type glob -i '*.png' -c:v libx264 in.mp4
-
-
 mencoder -fps $fps/1.001 -oac copy -ovc copy -o FINAL.mp4 in.mp4
 
 rm *.jpg
